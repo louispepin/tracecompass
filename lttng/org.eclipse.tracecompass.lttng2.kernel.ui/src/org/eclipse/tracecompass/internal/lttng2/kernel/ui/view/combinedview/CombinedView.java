0c6d1da6c3651ed80539c7decab98713879e52a2
louispepin <louis.pepin.ce@gmail.com>
2015-06-18 18:58:07 -0400

    Added statesystem explorer and eventlistener to combined view

---
+++ lttng/org.eclipse.tracecompass.lttng2.kernel.ui/src/org/eclipse/tracecompass/internal/lttng2/kernel/ui/view/combinedview/CombinedView.java
@@ -0,0 +1,36 @@
+package org.eclipse.tracecompass.internal.lttng2.kernel.ui.view.combinedview;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+/**
+ * Combine the Virtual Machine View and the Container view into a single class
+ *
+ * @author Louis Pepin
+ */
+public class CombinedView extends ViewPart {
+
+    CombinedViewer fViewer;
+
+    /** View ID. */
+    //private static final String ID = "org.eclipse.tracecompass.internal.lttng2.kernel.ui.view.combinedview"; //$NON-NLS-1$
+
+    /**
+     * Default constructor
+     */
+    public CombinedView() {
+        super();
+    }
+
+    @Override
+    public void createPartControl(Composite parent) {
+        fViewer = new CombinedViewer(parent);
+    }
+
+    @Override
+    public void setFocus() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
